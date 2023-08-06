9758c6ad9cbc6369df9b74fe8e6f425252568503
Yaroslav Abaturov <yarizzer@gmail.com>
2023-08-06 12:03:30 +0200

    AppCore prepared

---
+++ StaringContestTrainer/Application/AppCore/AppCore.swift
@@ -0,0 +1,35 @@
+//
+//  AppCore.swift
+//  StaringContestTrainer
+//
+//  Created by Yaroslav Abaturov on 6.08.2023.
+//
+
+class AppCore {
+    static let shared = AppCore()
+    
+    private init() {
+//        self.appUIL = AppUILayer()
+//        self.appCIL = AppCIProcessorLayer()
+    }
+    
+//    private let appUIL: AppUILayerType
+//    private let appCIL: AppCIProcessorLayerType
+}
+
+extension AppCore: AppCoreSessionType {
+    func prepareSession(completion: @escaping (Bool) -> ()) {
+        writeLog(type: .info, message: "Session prepared")
+        completion(true)
+    }
+    func openSession() { writeLog(type: .info, message: "Session opened") }
+    func closeSession() { writeLog(type: .info, message: "Session closed") }
+}
+
+extension AppCore: AppCoreUILayerType {
+//    var uiLayer: AppUILayerType { appUIL }
+}
+
+extension AppCore: AppCoreCIProcessorLayerType {
+//    var ciProcessorLayer: AppCIProcessorLayerType { appCIL }
+}
---
+++ StaringContestTrainer/Application/AppCore/AppCoreContract.swift
@@ -0,0 +1,20 @@
+//
+//  AppCoreContract.swift
+//  StaringContestTrainer
+//
+//  Created by Yaroslav Abaturov on 6.08.2023.
+//
+
+protocol AppCoreSessionType {
+    func prepareSession(completion: @escaping (Bool) -> ())
+    func openSession()
+    func closeSession()
+}
+
+protocol AppCoreUILayerType {
+//    var uiLayer: AppUILayerType { get }
+}
+
+protocol AppCoreCIProcessorLayerType {
+//    var ciProcessorLayer: AppCIProcessorLayerType { get }
+}
