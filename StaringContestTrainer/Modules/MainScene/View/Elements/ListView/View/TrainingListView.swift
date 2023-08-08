ae93203d73338291a0d631bf31bf1e8ff305094e
Yaroslav Abaturov <yarizzer@gmail.com>
2023-08-08 18:33:10 +0200

    cell preparation

--- StaringContestTrainer/Modules/MainScene/View/Elements/ListView/View/TrainingListView.swift
+++ StaringContestTrainer/Modules/MainScene/View/Elements/ListView/View/TrainingListView.swift
@@ -31,36 +31,39 @@ class TrainingListView: UIView {
     }
     
     private func setupProvider() {
-        provider = TableViewProvider(for: self.tableView, with: model as! TableViewProviderViewModel)
-        provider?.registerCells([ItemSelectionSceneRoleTableViewCell.self,
-                                 ItemSelectionSceneObjectTableViewCell.self])
+        guard let model = viewModel, let tableView else { return }
+        
+        provider = TableViewProvider(for: tableView, with: model as! TableViewProviderViewModel)
+        provider?.registerCells([TrainingListTableViewCell.self])
         
         provider?.onConfigureCell = { [weak self] indexPath in
             guard let provider = self?.provider else { return UITableViewCell() }
 
-            switch model.currentType {
-            case .roleSelection:
-                let cell: ItemSelectionSceneRoleTableViewCell = provider.dequeueReusableCell(for: indexPath)
-
-                guard let cellModel = model.getCellViewModel(with: indexPath.row) else { return cell }
-
-                cell.setup(with: cellModel)
-
-                return cell
-            case .objectSelection:
-                let cell: ItemSelectionSceneObjectTableViewCell = provider.dequeueReusableCell(for: indexPath)
-
-                guard let cellModel = model.getCellViewModel(with: indexPath.row) else { return cell }
-
-                cell.setup(with: cellModel)
-
-                return cell
-            default: return UITableViewCell()
-            }
+//            switch model.currentType {
+//            case .roleSelection:
+//                let cell: ItemSelectionSceneRoleTableViewCell = provider.dequeueReusableCell(for: indexPath)
+//
+//                guard let cellModel = model.getCellViewModel(with: indexPath.row) else { return cell }
+//
+//                cell.setup(with: cellModel)
+//
+//                return cell
+//            case .objectSelection:
+//                let cell: ItemSelectionSceneObjectTableViewCell = provider.dequeueReusableCell(for: indexPath)
+//
+//                guard let cellModel = model.getCellViewModel(with: indexPath.row) else { return cell }
+//
+//                cell.setup(with: cellModel)
+//
+//                return cell
+//            default: return UITableViewCell()
+//            }
+            #warning("replace with real one")
+            return UITableViewCell()
         }
 
         provider?.onSelectCell = { [weak self] indexPath in
-            self?.interactor?.makeRequest(requestType: .itemSelected(withIndex: indexPath.row))
+            #warning("replace with model notification")
         }
     }
     
