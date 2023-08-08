04fc20b81dea62e3ee35bdb36fcc1bd801245714
Yaroslav Abaturov <yarizzer@gmail.com>
2023-08-08 12:18:43 +0200

    TrainingListView created

---
+++ StaringContestTrainer/Modules/MainScene/View/Elements/ListView/View/TrainingListView.swift
@@ -0,0 +1,15 @@
+//
+//  TrainingListView.swift
+//  StaringContestTrainer
+//
+//  Created by Yaroslav Abaturov on 8.08.2023.
+//
+
+import UIKit
+
+class TrainingListView: UIView {
+    
+    
+//    private var provider: TableViewProviderType?
+//    private var tableView: UITableView?
+}
